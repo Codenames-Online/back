c7c49373dac94180abaf58ac4cd667f04cc92a40
Armaan Shah <armaanshah96@gmail.com>
2017-11-04 14:58:08 -0500

    temp comit

---
+++ lib/components/Game.d.ts
@@ -0,0 +1,10 @@
+export declare class Game {
+    private score;
+    private clue;
+    private numGuesses;
+    private turn;
+    private board;
+    private players;
+    private startTeam;
+    constructor();
+}
