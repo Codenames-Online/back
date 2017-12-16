546641c67a5dce9f8a4d17ab11d973fe7100d7b8
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-12-15 18:02:04 -0600

    Add Game Utility unit tests

--- test/board.test.ts
+++ test/board.test.ts
@@ -1,28 +1,27 @@
-import { Board } from '../src/Board'
-import { Team, Color } from '../src/constants/Constants'
+import { Board } from '../src/Board';
+import { Team, Color } from '../src/constants/Constants';
 
 import 'mocha';
 import { expect } from 'chai'
 
 describe("Filename: board.test.ts:\n\nThe board", () => {
-    it("should have expected number of colors with expected distribution", () => {
-        let board = new Board(Team.blue);
+  it("should have expected number of colors with expected distribution", () => {
+    let board = new Board(Team.blue);
+    
+    let num_red = board.colors.filter(n => n === Color.red).length;
+    let num_blue = board.colors.filter(n => n === Color.blue).length;
+    let num_neutral = board.colors.filter(n => n === Color.neutral).length;
+    let num_assassins = board.colors.filter(n => n === Color.assassin).length;
+    
+    expect(board.colors.length).to.equal(25);
+    expect(num_red).to.be.within(8, 9);
+    expect(num_blue).to.be.within(8, 9);
+    expect(num_neutral).to.equal(7);
+    expect(num_assassins).to.equal(1);
+  });
 
-        let num_red = board.colors.filter(n => n === Color.red).length;
-        let num_blue = board.colors.filter(n => n === Color.blue).length;
-        let num_neutral = board.colors.filter(n => n === Color.neutral).length;
-        let num_assassins = board.colors.filter(n => n === Color.assassin).length;
-        
-        expect(board.colors.length).to.equal(25);
-        expect(num_red).to.be.within(8, 9);
-        expect(num_blue).to.be.within(8, 9);
-        expect(num_neutral).to.equal(7);
-        expect(num_assassins).to.equal(1);
-    });
-
-    it("should have expected number of cards", () => {
-        let board = new Board(Team.red);
-
-        expect(board.cards.length).to.equal(25);
-    });
+  it("should have expected number of cards", () => {
+    let board = new Board(Team.red);
+    expect(board.cards.length).to.equal(25);
+  });
 });
---
+++ test/game_utility.test.ts
@@ -0,0 +1,65 @@
+import { SPlayer } from '../src/SPlayer';
+import { SLoiterer } from '../src/SLoiterer';
+import { SSpymaster } from '../src/SSpymaster';
+import { SOperative } from '../src/SOperative';
+import { GameUtility as gu } from '../src/GameUtility';
+import { Color, Team, Turn } from '../src/constants/Constants';
+
+import 'mocha';
+import { expect } from 'chai';
+import WebSocket = require('ws')
+import { mock, instance, when } from 'ts-mockito';
+
+describe("Filename: game_utility.test.ts:\n\nGame Utility", () => {
+	let mock_ws: WebSocket;
+	let mock_ws_instance: WebSocket;
+	let loiterers: SLoiterer[];
+	let players: SPlayer[];
+
+	before(() => {
+		mock_ws = mock(WebSocket);
+		mock_ws_instance = instance(mock_ws);
+
+		let red_l_one = new SLoiterer("red", "1", Team.red, mock_ws_instance);
+		let red_l_two = new SLoiterer("red", "2", Team.red, mock_ws_instance);
+		let blue_l_one = new SLoiterer("blue", "1", Team.blue, mock_ws_instance);
+		let blue_l_two = new SLoiterer("blue", "2", Team.blue, mock_ws_instance);
+		let blue_l_three = new SLoiterer("blue", "3", Team.blue, mock_ws_instance);
+		loiterers = [red_l_one, red_l_two, blue_l_one, blue_l_two, blue_l_three];
+
+		let red_p_one = new SSpymaster("red", "1", Team.red, mock_ws_instance, Turn.spy);
+		let red_p_two = new SOperative("red", "2", Team.red, mock_ws_instance, Turn.op);
+		let blue_p_one = new SSpymaster("blue", "1", Team.blue, mock_ws_instance, Turn.spy);
+		let blue_p_two = new SOperative("blue", "2", Team.blue, mock_ws_instance, Turn.op);
+		let blue_p_three = new SOperative("blue", "3", Team.blue, mock_ws_instance, Turn.op);
+		players = [red_p_one, red_p_two, blue_p_one, blue_p_two, blue_p_three];
+	});
+
+  it("should correctly split loiterers on team", () => {
+		let teams = gu.getSloitererTeams(loiterers);
+
+		expect(teams.red.length).to.equal(2);
+		expect(teams.blue.length).to.equal(3);
+	});
+	
+  it("should correctly split players on team", () => {
+		let teams = gu.getPlayerTeams(players);
+
+		expect(teams.red.length).to.equal(2);
+		expect(teams.blue.length).to.equal(3);
+	});
+	
+	it("should get correct number of names from sloiterers", () => {
+		let team_names = gu.getSloitererRoster(players);
+
+		expect(team_names[Team.red].length).to.equal(2);
+		expect(team_names[Team.blue].length).to.equal(3);
+	});
+	
+	it("should get correct number of names from splayers", () => {
+		let team_names = gu.getPlayerRoster(players);
+
+		expect(team_names[Team.red].length).to.equal(2);
+		expect(team_names[Team.blue].length).to.equal(3);
+  });
+});
