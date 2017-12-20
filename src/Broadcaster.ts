b4b6a857c8b22e17e434aeb5167996d36a2d1ca8
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-12-20 17:47:34 -0600

    Clean up s prefix and remove player

--- src/Broadcaster.ts
+++ src/Broadcaster.ts
@@ -9,58 +9,50 @@ import { Player } from './Player';
 
 export module Broadcaster {
 	function broadcastToPlayers(players: Player[], message: Object) {
-		for(let player of players) { sendToPlayer(player, message); }
+		players.forEach(player => sendToPlayer(player, message));
 	}
 
 	function sendToPlayer(player: Player, message: Object) {
 		player.socket.send(JSON.stringify(message));
 	}
 
-	function broadcastToSloiterers(sloiterers: Loiterer[], message: Object) {
-		for(let sloiterer of sloiterers) { sendToSloiterer(sloiterer, message); }
+	export function switchTurn(agents: Agent[], team: Team, turn: Turn) {
+		broadcastToPlayers(agents, { action: "switchTurn", team: team, turn: turn });
 	}
 
-	function sendToSloiterer(sloiterer: Loiterer, message: Object) {
-		sloiterer.socket.send(JSON.stringify(message));
+	export function switchActiveTeam(agents: Agent[], team: Team, turn: Turn) {
+		broadcastToPlayers(agents, { action: "switchActiveTeam", team: team, turn: turn });
 	}
 
-	export function switchTurn(players: Agent[], team: Team, turn: Turn) {
-		broadcastToPlayers(players, { action: "switchTurn", team: team, turn: turn });
+	export function postClue(agents: Agent[], clue: Clue, team: Team) {
+		broadcastToPlayers(agents, { action: "postClue", clue: clue, team: team });
 	}
 
-	export function switchActiveTeam(players: Agent[], team: Team, turn: Turn) {
-		broadcastToPlayers(players, { action: "switchActiveTeam", team: team, turn: turn });
+	export function generateCards(agents: Agent[], board: Board) {
+		broadcastToPlayers(agents, { action: "generateCards", board: Board });
 	}
 
-	export function postClue(players: Agent[], clue: Clue, team: Team) {
-		broadcastToPlayers(players, { action: "postClue", clue: clue, team: team });
-	}
-
-	export function generateCards(players: Agent[], board: Board) {
-		broadcastToPlayers(players, { action: "generateCards", board: Board });
-	}
-
-	export function updateTeams(sloiterers: Loiterer[], roster: [string[], string[]]) {
-		broadcastToSloiterers(sloiterers, { 
+	export function updateTeams(loiterers: Loiterer[], roster: [string[], string[]]) {
+		broadcastToPlayers(loiterers, { 
 			action: "updateTeams",
 			teams: { blue: roster[Team.blue], red: roster[Team.red] },
 		});
 	}
 
-	export function toggleStartButton(sloiterers: Loiterer[], canEnable: boolean) {
-		broadcastToSloiterers(sloiterers, { action: "toggleStartButton", enable: canEnable });
+	export function toggleStartButton(loiterers: Loiterer[], canEnable: boolean) {
+		broadcastToPlayers(loiterers, { action: "toggleStartButton", enable: canEnable });
 	}
 
-export function updateBoard(splayers: Agent[], board: [number, Card][]) {
-		broadcastToPlayers(splayers, { action: "updateBoard", board: board });
+	export function updateBoard(agents: Agent[], board: [number, Card][]) {
+		broadcastToPlayers(agents, { action: "updateBoard", board: board });
 	}
 
-	export function updateScore(splayers: Agent[], score: number[]) {
-		broadcastToPlayers(splayers, { action: "updateScore", score: score });
+	export function updateScore(agents: Agent[], score: number[]) {
+		broadcastToPlayers(agents, { action: "updateScore", score: score });
 	}
 
-	export function sendMessage(players: Agent[], chat: string, player) {
-		broadcastToPlayers(players, {
+	export function sendMessage(agents: Agent[], chat: string, player) {
+		broadcastToPlayers(agents, {
 			action: "sendMessage",
 			text: chat,
 			playerTeam: player.team,
@@ -68,32 +60,32 @@ export function updateBoard(splayers: Agent[], board: [number, Card][]) {
 		});
 	}
 
-	export function startGame(splayers: Agent[], startTeam: Team, startingRoster) {
-		broadcastToPlayers(splayers, { 
+	export function startGame(agents: Agent[], startTeam: Team, startingRoster) {
+		broadcastToPlayers(agents, { 
 			action: "gameStarted", startTeam: startTeam, roster: startingRoster
 		});
 	}
 
-	export function endGame(splayers: Agent[], team: Team) {
-		broadcastToPlayers(splayers, { action: "endGame", team: team });
+	export function endGame(agents: Agent[], team: Team) {
+		broadcastToPlayers(agents, { action: "endGame", team: team });
 	}
 
 	// PRIVATE
 
-	export function allowGuess(players: Agent[], bool: boolean) {
-		broadcastToPlayers(players, { action: "allowGuess", bool: bool });
+	export function allowGuess(agents: Agent[], bool: boolean) {
+		broadcastToPlayers(agents, { action: "allowGuess", bool: bool });
 	}
 
 	export function updateLoner(loner: Player) {
 		sendToPlayer(loner, { action: "updateLoner", person: loner });
 	}
 
-	export function updateLoiterer(sloiterer: Loiterer, gid: string) {
-		sendToSloiterer(sloiterer, { action: "updateLoiterer", person: sloiterer, gid: gid });
+	export function updateLoiterer(loiterer: Loiterer, gid: string) {
+		sendToPlayer(loiterer, { action: "updateLoiterer", person: loiterer, gid: gid });
 	}
 
-	export function updateLoitererToAgent(sloiterer: Loiterer, splayer: Agent) {
-		sendToSloiterer(sloiterer, { action: "updateLoitererToPlayer", player: splayer });
+	export function updateLoitererToAgent(loiterer: Loiterer, agent: Agent) {
+		sendToPlayer(loiterer, { action: "updateLoitererToPlayer", player: agent });
 	}
 
 	export function promptForClue(spymaster: Spymaster) {
