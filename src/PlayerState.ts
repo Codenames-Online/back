4923f68ec6f26f57c472143352cad031f5fcee47
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-12-20 13:11:30 -0600

    Remove player from lobby

--- src/PlayerState.ts
+++ src/PlayerState.ts
@@ -11,7 +11,12 @@ export class PlayerState {
 	}
 
 	getPid(): string { return this.pid; }
-	getGid(): string | undefined { return this.gid; }
+	getGid(): string {
+		if(!this.gid)
+			throw new Error('Attempted to access gid of player not yet in lobby or game');
+		
+		return this.gid;
+	}
 	getLoc(): PlayerLocation { return this.loc; }
 
 	placeInLobby(gid: string) {
@@ -24,7 +29,7 @@ export class PlayerState {
 
 	placeInGame() {
 		if(this.loc !== PlayerLocation.lobby)
-		throw new Error("Player must be in lobby to place in game");
+			throw new Error("Player must be in lobby to place in game");
 	
 		this.loc = PlayerLocation.game;
 	}
