bb345407a19f9e0a82aefd1d6e7cfa834972be37
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-12-21 00:40:59 -0600

    Add getSpymaster tests

--- test/game_utility.test.ts
+++ test/game_utility.test.ts
@@ -22,18 +22,18 @@ describe("Filename: game_utility.test.ts:\n\nGame Utility", () => {
 		mock_ws = mock(WebSocket);
 		mock_ws_instance = instance(mock_ws);
 
-		let red_l_one = new Loiterer("red", "1", mock_ws_instance, Team.red);
-		let red_l_two = new Loiterer("red", "2", mock_ws_instance, Team.red);
-		let blue_l_one = new Loiterer("blue", "1", mock_ws_instance, Team.blue);
-		let blue_l_two = new Loiterer("blue", "2", mock_ws_instance, Team.blue);
-		let blue_l_three = new Loiterer("blue", "3", mock_ws_instance, Team.blue);
+		let red_l_one = new Loiterer("1", "red_one", mock_ws_instance, Team.red);
+		let red_l_two = new Loiterer("2", "red_two", mock_ws_instance, Team.red);
+		let blue_l_one = new Loiterer("1", "blue_one", mock_ws_instance, Team.blue);
+		let blue_l_two = new Loiterer("2", "blue_two", mock_ws_instance, Team.blue);
+		let blue_l_three = new Loiterer("3", "blue_three", mock_ws_instance, Team.blue);
 		loiterers = [red_l_one, red_l_two, blue_l_one, blue_l_two, blue_l_three];
 
-		let red_p_one = new Spymaster("red", "1", mock_ws_instance, Team.red);
-		let red_p_two = new Operative("red", "2", mock_ws_instance, Team.red);
-		let red_p_three = new Operative("red", "3", mock_ws_instance, Team.red);
-		let blue_p_one = new Spymaster("blue", "1", mock_ws_instance, Team.blue);
-		let blue_p_two = new Operative("blue", "2", mock_ws_instance, Team.blue);
+		let red_p_one = new Spymaster("1", "red_spy", mock_ws_instance, Team.red);
+		let red_p_two = new Operative("2", "red_op_one", mock_ws_instance, Team.red);
+		let red_p_three = new Operative("3", "red_op_two", mock_ws_instance, Team.red);
+		let blue_p_one = new Spymaster("1", "blue_spy", mock_ws_instance, Team.blue);
+		let blue_p_two = new Operative("2", "blue_op_one", mock_ws_instance, Team.blue);
 		agents = [red_p_one, red_p_two, red_p_three, blue_p_one, blue_p_two];
 	});
 
@@ -74,5 +74,11 @@ describe("Filename: game_utility.test.ts:\n\nGame Utility", () => {
 		let ops: Teams<Operative> = gu.getOperatives(agents);
 		expect(ops.red.length).to.equal(2);
 		expect(ops.blue.length).to.equal(1);
+	});
+
+	it("should correctly split spymasters on team", () => {
+		let spys: Teams<Spymaster> = gu.getSpymasters(agents);
+		expect((spys.red.pop() as Spymaster).name).to.equal("red_spy");
+		expect((spys.blue.pop() as Spymaster).name).to.equal("blue_spy");
 	});
 });
