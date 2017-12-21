f447cb89cecbb430f0de402f47e447caa0d08474
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-12-21 00:04:22 -0600

    Add methods for getting agents by team and role

--- src/GameUtility.ts
+++ src/GameUtility.ts
@@ -1,7 +1,9 @@
-import { Teams } from './Teams'
+import { Agent } from './Agent';
+import { Teams } from './Teams';
 import { Operative } from './Operative';
+import { Spymaster } from './Spymaster';
 import { TeamPlayer } from './TeamPlayer';
-import { Team } from './constants/Constants'
+import { Team, Turn } from './constants/Constants'
 
 export module GameUtility {	
 	export function getTeams<T extends TeamPlayer>(teamPlayers: T[]): Teams<T> {
@@ -22,5 +24,26 @@ export module GameUtility {
 
 	export function getTeamOps(allOps: Operative[], team: Team): Operative[] {
 		return allOps.filter(op => op.team === team);
+	}
+
+	// takes an object which extends the hasRedAndBlue interface meaning it has
+	// a field 'red' and a field 'blue' both of which are arrays of objects
+	// which extend TeamPlayer. hasRedAndBlue needs a Type argument so we
+	// pass the type of an element in the 'red' field which will be an object
+	// that extends TeamPlayer. Finally we want to return an array of this type
+	export function getByTeam<T extends Teams<T["red"][0]>>(teams: T, team: Team): T["red"] {
+		return team === Team.red ? teams.red : teams.blue;
+	}
+
+	export function getOperatives(agents: Agent[]): Teams<Operative> {
+		return getTeams(getAgentByRole(agents, Turn.op) as Operative[]);
+	}
+
+	export function getSpymasters(agents: Agent[]): Teams<Spymaster> {
+		return getTeams(getAgentByRole(agents, Turn.spy) as Spymaster[]);
+	}
+
+	function getAgentByRole(agents: Agent[], role: Turn): Agent[] {
+		return agents.filter(agent => agent.role === role);
 	}
 }
