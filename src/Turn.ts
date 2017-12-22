af18d290f9302513affe925eddb7dcc9f8bf59e0
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-12-21 19:54:55 -0600

    Implement start on GameTurn

--- src/Turn.ts
+++ src/Turn.ts
@@ -1,13 +1,18 @@
 import { Agent } from "./Agent";
+import { Spymaster } from "./Spymaster";
+import { Broadcaster } from "./Broadcaster";
+import { GameUtility as gu } from "./GameUtility";
 import { Team, Turn } from "./constants/Constants";
 
 export class GameTurn {
 	private role: Turn;
 	private team: Team;
+	private started: Boolean;
 
 	constructor(startTeam: Team) {
 		this.team = startTeam;
 		this.role = Turn.spy;
+		this.started = false;
 	}
 
 	getRole(): Turn { return this.role; }
@@ -19,6 +24,12 @@ export class GameTurn {
 	}
 
 	start(agents: Agent[]) {
-		
+		if(this.started) { throw new Error("Can't start game twice"); }
+
+		this.started = true;
+		let spyArr = gu.getByTeam(gu.getSpymasters(agents), this.team);
+
+		Broadcaster.startGame(agents, this.team, gu.getStartingRoster(agents));
+		Broadcaster.promptForClue(spyArr.pop() as Spymaster)
 	}
 }
