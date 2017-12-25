3851e08ac812423c807fb75e5cd49a3880a9ad57
Nathan Shelly <nathanshelly@u.northwestern.edu>
2017-12-24 19:33:47 -0600

    Convert all indentation to spaces in tests

--- test/game_utility.test.ts
+++ test/game_utility.test.ts
@@ -14,84 +14,84 @@ import { mock, instance } from 'ts-mockito';
 
 
 describe("Filename: game_utility.test.ts:\n\nGame Utility", () => {
-	let mock_ws_instance: ws;
-	let loiterers: Loiterer[];
-	let agents: Agent[];
+  let mock_ws_instance: ws;
+  let loiterers: Loiterer[];
+  let agents: Agent[];
 
-	before(() => {
-		mock_ws_instance = instance(mock(ws));
+  before(() => {
+    mock_ws_instance = instance(mock(ws));
 
-		let red_l_one = new Loiterer("1", "red_one", mock_ws_instance, Team.red);
-		let red_l_two = new Loiterer("2", "red_two", mock_ws_instance, Team.red);
-		let blue_l_one = new Loiterer("1", "blue_one", mock_ws_instance, Team.blue);
-		let blue_l_two = new Loiterer("2", "blue_two", mock_ws_instance, Team.blue);
-		let blue_l_three = new Loiterer("3", "blue_three", mock_ws_instance, Team.blue);
-		loiterers = [red_l_one, red_l_two, blue_l_one, blue_l_two, blue_l_three];
+    let red_l_one = new Loiterer("1", "red_one", mock_ws_instance, Team.red);
+    let red_l_two = new Loiterer("2", "red_two", mock_ws_instance, Team.red);
+    let blue_l_one = new Loiterer("1", "blue_one", mock_ws_instance, Team.blue);
+    let blue_l_two = new Loiterer("2", "blue_two", mock_ws_instance, Team.blue);
+    let blue_l_three = new Loiterer("3", "blue_three", mock_ws_instance, Team.blue);
+    loiterers = [red_l_one, red_l_two, blue_l_one, blue_l_two, blue_l_three];
 
-		let red_p_one = new Spymaster("1", "red_spy", mock_ws_instance, Team.red);
-		let red_p_two = new Operative("2", "red_op_one", mock_ws_instance, Team.red);
-		let red_p_three = new Operative("3", "red_op_two", mock_ws_instance, Team.red);
-		let blue_p_one = new Spymaster("1", "blue_spy", mock_ws_instance, Team.blue);
-		let blue_p_two = new Operative("2", "blue_op_one", mock_ws_instance, Team.blue);
-		agents = [red_p_one, red_p_two, red_p_three, blue_p_one, blue_p_two];
-	});
+    let red_p_one = new Spymaster("1", "red_spy", mock_ws_instance, Team.red);
+    let red_p_two = new Operative("2", "red_op_one", mock_ws_instance, Team.red);
+    let red_p_three = new Operative("3", "red_op_two", mock_ws_instance, Team.red);
+    let blue_p_one = new Spymaster("1", "blue_spy", mock_ws_instance, Team.blue);
+    let blue_p_two = new Operative("2", "blue_op_one", mock_ws_instance, Team.blue);
+    agents = [red_p_one, red_p_two, red_p_three, blue_p_one, blue_p_two];
+  });
 
   it("should correctly split loiterers on team", () => {
-		let teams = gu.getTeams(loiterers);
+    let teams = gu.getTeams(loiterers);
 
-		expect(teams.red.length).to.equal(2);
-		expect(teams.blue.length).to.equal(3);
-	});
-	
+    expect(teams.red.length).to.equal(2);
+    expect(teams.blue.length).to.equal(3);
+  });
+  
   it("should correctly split players on team", () => {
-		let teams = gu.getTeams(agents);
+    let teams = gu.getTeams(agents);
 
-		expect(teams.red.length).to.equal(3);
-		expect(teams.blue.length).to.equal(2);
-	});
-	
-	it("should get correct number of names from loiterers", () => {
-		let team_names = gu.getRoster(gu.getTeams(loiterers));
+    expect(teams.red.length).to.equal(3);
+    expect(teams.blue.length).to.equal(2);
+  });
+  
+  it("should get correct number of names from loiterers", () => {
+    let team_names = gu.getRoster(gu.getTeams(loiterers));
  
-		expect(team_names[Team.red].length).to.equal(2);
-		expect(team_names[Team.blue].length).to.equal(3);
-	});
-	
-	it("should get correct number of names from players", () => {
-		let team_names = gu.getRoster(gu.getTeams(agents));
+    expect(team_names[Team.red].length).to.equal(2);
+    expect(team_names[Team.blue].length).to.equal(3);
+  });
+  
+  it("should get correct number of names from players", () => {
+    let team_names = gu.getRoster(gu.getTeams(agents));
 
-		expect(team_names[Team.red].length).to.equal(3);
-		expect(team_names[Team.blue].length).to.equal(2);
-	});
-	
-	it("should correctly return other team", () => {
-		expect(gu.getOtherTeam(Team.red)).to.equal(Team.blue);
-		expect(gu.getOtherTeam(Team.blue)).to.equal(Team.red);
-	});
-	
-	it("should correctly split operatives on team", () => {
-		let ops: Teams<Operative> = gu.getOperatives(agents);
-		expect(ops.red.length).to.equal(2);
-		expect(ops.blue.length).to.equal(1);
-	});
+    expect(team_names[Team.red].length).to.equal(3);
+    expect(team_names[Team.blue].length).to.equal(2);
+  });
+  
+  it("should correctly return other team", () => {
+    expect(gu.getOtherTeam(Team.red)).to.equal(Team.blue);
+    expect(gu.getOtherTeam(Team.blue)).to.equal(Team.red);
+  });
+  
+  it("should correctly split operatives on team", () => {
+    let ops: Teams<Operative> = gu.getOperatives(agents);
+    expect(ops.red.length).to.equal(2);
+    expect(ops.blue.length).to.equal(1);
+  });
 
-	it("should correctly split spymasters on team", () => {
-		let spys: Teams<Spymaster> = gu.getSpymasters(agents);
-		expect((spys.red.pop() as Spymaster).name).to.equal("red_spy");
-		expect((spys.blue.pop() as Spymaster).name).to.equal("blue_spy");
-	});
+  it("should correctly split spymasters on team", () => {
+    let spys: Teams<Spymaster> = gu.getSpymasters(agents);
+    expect((spys.red.pop() as Spymaster).name).to.equal("red_spy");
+    expect((spys.blue.pop() as Spymaster).name).to.equal("blue_spy");
+  });
 
-	it("should correctly split spymasters on team", () => {
-		let ops: Teams<Operative> = gu.getOperatives(agents);
+  it("should correctly split spymasters on team", () => {
+    let ops: Teams<Operative> = gu.getOperatives(agents);
 
-		expect(gu.getByTeam(ops, Team.red).length).to.equal(2);
-		expect(gu.getByTeam(ops, Team.blue).length).to.equal(1);
-	});
-	
-	it("should eventually produce both starting team colors", () => {
-		let toFind = gu.getOtherTeam(gu.getStartTeam());
-		// test passes as long as this loop doesn't hang forever
-		// due to randomness this test can be reliably expected to not hang for too long
-		while(toFind !== gu.getStartTeam()) {}
-	});
+    expect(gu.getByTeam(ops, Team.red).length).to.equal(2);
+    expect(gu.getByTeam(ops, Team.blue).length).to.equal(1);
+  });
+  
+  it("should eventually produce both starting team colors", () => {
+    let toFind = gu.getOtherTeam(gu.getStartTeam());
+    // test passes as long as this loop doesn't hang forever
+    // due to randomness this test can be reliably expected to not hang for too long
+    while(toFind !== gu.getStartTeam()) {}
+  });
 });
--- test/turn.test.ts
+++ test/turn.test.ts
@@ -12,117 +12,117 @@ import { expect } from 'chai';
 import { mock, instance, when, verify, deepEqual, resetCalls } from 'ts-mockito';
 
 describe("Filename: turn.test.ts:\n\nTurn", () => {
-	let mock_ws: ws;
-	let ws_inst: ws;
-	let agents: Agent[];
-	let turn: Turn;
+  let mock_ws: ws;
+  let ws_inst: ws;
+  let agents: Agent[];
+  let turn: Turn;
 
-	before(() => {
-		mock_ws = mock(ws);
-		ws_inst = instance(mock_ws);
+  before(() => {
+    mock_ws = mock(ws);
+    ws_inst = instance(mock_ws);
 
-		let data: [string, string, Team, Role][] = [["1", "red_spy", Team.red, Role.spy],
-		["2", "red_op_one", Team.red, Role.op], ["3", "red_op_two", Team.red, Role.op],
-		["1", "blue_spy", Team.blue, Role.spy], ["2", "blue_op_one", Team.blue, Role.op]]
-		
-		agents = data.map(t =>
-			new (t[3] === Role.spy ? Spymaster : Operative)(t[0], t[1], ws_inst, t[2])
-		);
-	});
+    let data: [string, string, Team, Role][] = [["1", "red_spy", Team.red, Role.spy],
+    ["2", "red_op_one", Team.red, Role.op], ["3", "red_op_two", Team.red, Role.op],
+    ["1", "blue_spy", Team.blue, Role.spy], ["2", "blue_op_one", Team.blue, Role.op]]
+    
+    agents = data.map(t =>
+      new (t[3] === Role.spy ? Spymaster : Operative)(t[0], t[1], ws_inst, t[2])
+    );
+  });
 
-	beforeEach(() => {
-		turn = new Turn(Team.red);
-		resetCalls(mock_ws);
-	});
+  beforeEach(() => {
+    turn = new Turn(Team.red);
+    resetCalls(mock_ws);
+  });
 
-	it("should always begin with spy of starting team", () => {	
-		expect(turn.getRole()).to.equal(Role.spy);
-		expect(turn.getTeam()).to.equal(Team.red);
-	});
+  it("should always begin with spy of starting team", () => {	
+    expect(turn.getRole()).to.equal(Role.spy);
+    expect(turn.getTeam()).to.equal(Team.red);
+  });
 
-	it("should error on starting twice", () => {	
-		turn.start(agents);
-		expect(() => { turn.start(agents) }).to.throw(Error, "Can't start game twice");
-	});
+  it("should error on starting twice", () => {	
+    turn.start(agents);
+    expect(() => { turn.start(agents) }).to.throw(Error, "Can't start game twice");
+  });
 
-	it("should error on advancing without first starting", () => {	
-		expect(() => { turn.advance(agents) }).to.throw(Error, "Can't advance without starting");
-	});
+  it("should error on advancing without first starting", () => {	
+    expect(() => { turn.advance(agents) }).to.throw(Error, "Can't advance without starting");
+  });
 
-	it("should correctly change teams and roles on advance", () => {
-		turn.start(agents);
+  it("should correctly change teams and roles on advance", () => {
+    turn.start(agents);
 
-		expect(turn.getRole()).to.equal(Role.spy);
-		expect(turn.getTeam()).to.equal(Team.red);
+    expect(turn.getRole()).to.equal(Role.spy);
+    expect(turn.getTeam()).to.equal(Team.red);
 
-		turn.advance(agents);
+    turn.advance(agents);
 
-		expect(turn.getRole()).to.equal(Role.op);
-		expect(turn.getTeam()).to.equal(Team.red);
+    expect(turn.getRole()).to.equal(Role.op);
+    expect(turn.getTeam()).to.equal(Team.red);
 
-		turn.advance(agents);
+    turn.advance(agents);
 
-		expect(turn.getRole()).to.equal(Role.spy);
-		expect(turn.getTeam()).to.equal(Team.blue);
+    expect(turn.getRole()).to.equal(Role.spy);
+    expect(turn.getTeam()).to.equal(Team.blue);
 
-		turn.advance(agents);
+    turn.advance(agents);
 
-		expect(turn.getRole()).to.equal(Role.op);
-		expect(turn.getTeam()).to.equal(Team.blue);
+    expect(turn.getRole()).to.equal(Role.op);
+    expect(turn.getTeam()).to.equal(Team.blue);
 
-		turn.advance(agents);
+    turn.advance(agents);
 
-		expect(turn.getRole()).to.equal(Role.spy);
-		expect(turn.getTeam()).to.equal(Team.red);
-	});
+    expect(turn.getRole()).to.equal(Role.spy);
+    expect(turn.getTeam()).to.equal(Team.red);
+  });
 
-	it("should send expected messages to expected agents on start", () => {
-		turn.start(agents);
+  it("should send expected messages to expected agents on start", () => {
+    turn.start(agents);
 
-		// should have sent prompt for clue to red spymaster (for now just confirming
-		// that it was sent to one agent)
-		verify(mock_ws.send(deepEqual(JSON.stringify(
-			{ action: "promptForClue" }
-		)))).once();
+    // should have sent prompt for clue to red spymaster (for now just confirming
+    // that it was sent to one agent)
+    verify(mock_ws.send(deepEqual(JSON.stringify(
+      { action: "promptForClue" }
+    )))).once();
 
-		// should have sent gameStarted message to all 5 agents
-		verify(mock_ws.send(deepEqual(JSON.stringify(
-			{ action: "gameStarted", startTeam: turn.getTeam(), roster: gu.getStartingRoster(agents) }
-		)))).times(5);
-	});
+    // should have sent gameStarted message to all 5 agents
+    verify(mock_ws.send(deepEqual(JSON.stringify(
+      { action: "gameStarted", startTeam: turn.getTeam(), roster: gu.getStartingRoster(agents) }
+    )))).times(5);
+  });
 
-	it("should send expected messages to expected agents on advance from spy to op", () => {
-		turn.start(agents);
+  it("should send expected messages to expected agents on advance from spy to op", () => {
+    turn.start(agents);
 
-		// state is red spy
-		turn.advance(agents);
+    // state is red spy
+    turn.advance(agents);
 
-		// state is red op
-		// should have sent switchTurn message to all 5 agents
-		verify(mock_ws.send(deepEqual(JSON.stringify(
-			{ action: "switchTurn", team: turn.getTeam(), turn: turn.getRole() }
-		)))).times(5);
-	});
+    // state is red op
+    // should have sent switchTurn message to all 5 agents
+    verify(mock_ws.send(deepEqual(JSON.stringify(
+      { action: "switchTurn", team: turn.getTeam(), turn: turn.getRole() }
+    )))).times(5);
+  });
 
-	it("should send expected messages to expected agents on advance from op to spy", () => {
-		turn.start(agents);
+  it("should send expected messages to expected agents on advance from op to spy", () => {
+    turn.start(agents);
 
-		// state is red spy
-		turn.advance(agents);
+    // state is red spy
+    turn.advance(agents);
 
-		// state is red op
-		turn.advance(agents);
+    // state is red op
+    turn.advance(agents);
 
-		// state is blue spy
-		// should have sent switchActiveTeam message to all 5 agents
-		verify(mock_ws.send(deepEqual(JSON.stringify(
-			{ action: "switchActiveTeam", team: turn.getTeam(), turn: turn.getRole() }
-		)))).times(5);
+    // state is blue spy
+    // should have sent switchActiveTeam message to all 5 agents
+    verify(mock_ws.send(deepEqual(JSON.stringify(
+      { action: "switchActiveTeam", team: turn.getTeam(), turn: turn.getRole() }
+    )))).times(5);
 
-		// should also have sent promptForClue to blue spy (currently just checking
-		// that is was sent twice [once to start game and then once on switch])
-		verify(mock_ws.send(deepEqual(JSON.stringify(
-			{ action: "promptForClue" }
-		)))).twice();
-	});
+    // should also have sent promptForClue to blue spy (currently just checking
+    // that is was sent twice [once to start game and then once on switch])
+    verify(mock_ws.send(deepEqual(JSON.stringify(
+      { action: "promptForClue" }
+    )))).twice();
+  });
 });
